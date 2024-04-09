5d4c99bbfce1dda369d4cc8da042395a1e652d24
Joel Sita <joel.sita@genesys.com>
2024-04-09 10:26:44 +0100

    add event listers for document loading and button clicks

--- assets/js/scritpt.js
+++ assets/js/scritpt.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){
+let buttons = document.getElementsByTagName("button");
+
+  for  (let button of buttons){
+    button.addEventListener("click", function (){
+      if(this.getAttribute("data-tye") === "submit"){
+        alert("you clicked submit");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert(`you clicked ${gameType}`);
+      }
+    })
+  }
+
+})
+
 function runGame(){
 
 }
